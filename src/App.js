9924d798ad7b7a55d43f1aed6e683920b808fd51
LibirSoft <libir@outlook.com>
2021-05-13 03:28:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Route path="/" exact render={
+              () => {
+                return (<h1>HomePage</h1>)
+              }
+            } />
+            <Route path="/abaout" exact render={
+              () => {
+                return (<h1>AbaoutPage</h1>)
+              }
+            } />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
